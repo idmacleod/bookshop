b8025b726f3e9c0c1ce1e1405d73910fdec62053
idmacleod <idmacleod@gmail.com>
2020-04-24 16:07:54 +0100

    Added Author class, initialize, attr_accessor/attr_reader

---
+++ models/author.rb
@@ -0,0 +1,12 @@
+class Author
+
+    attr_accessor :first_name, :last_name
+    attr_reader :id
+
+    def initialize(options)
+        @id = options["id"].to_i if options["id"]
+        @first_name = options["first_name"]
+        @last_name = options["last_name"]
+    end
+
+end
