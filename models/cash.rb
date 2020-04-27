25631e588c0fe012234c02c6991a89c2751c9188
idmacleod <idmacleod@gmail.com>
2020-04-27 17:47:54 +0100

    Removing hard-coded £ from Cash class and including in books views instead

--- models/cash.rb
+++ models/cash.rb
@@ -1,13 +1,13 @@
 class Cash
 
     def self.in_pounds(amount_in_pence)
-        return "£0.00" if amount_in_pence == 0
+        return "0.00" if amount_in_pence == 0
         return "-" + Cash.in_pounds(-amount_in_pence) if amount_in_pence < 0
         pounds = amount_in_pence / 100
         remainder = amount_in_pence % 100
-        return "£#{pounds}.00" if remainder == 0
-        return "£#{pounds}.0#{remainder}" if remainder < 10
-        return "£#{pounds}.#{remainder}"
+        return "#{pounds}.00" if remainder == 0
+        return "#{pounds}.0#{remainder}" if remainder < 10
+        return "#{pounds}.#{remainder}"
     end
 
     def self.in_pence(amount_in_pounds)
