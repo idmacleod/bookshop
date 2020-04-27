b09eeb99ebc4f5a3528174ea54b5e0c31f44a39e
idmacleod <idmacleod@gmail.com>
2020-04-27 10:46:36 +0100

    Adding SHOW action to publishers_controller.rb

--- controllers/publishers_controller.rb
+++ controllers/publishers_controller.rb
@@ -10,3 +10,9 @@ get "/publishers" do
     @publishers = Publisher.all()
     erb(:"publishers/index")
 end
+
+# SHOW
+get "/publishers/:id" do
+    @publisher = Publisher.find(params[:id].to_i)
+    erb(:"publishers/show")
+end
